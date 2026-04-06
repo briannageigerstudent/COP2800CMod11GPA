ec174689f5752309fc1143eeaeae553b09befc77
Geiger <S3841751@students.fscj.edu>
2026-04-05 21:48:02 -0400

    added package statement

---
+++ edu/fscj/cop2800c/penguin/InvalidBirdDataException.java
@@ -0,0 +1,12 @@
+//InvalidBirdDataException.java
+// Brianna Geiger
+// 4/5/26
+// Exception file
+
+package edu.fscj.cop2800c.penguin;
+
+public class InvalidBirdDataException extends Exception {
+   public InvalidBirdDataException(String message) {
+      super(message);
+   }
+}
--- edu/fscj/cop2800c/penguin/PalmerPenguins.java
+++ edu/fscj/cop2800c/penguin/PalmerPenguins.java
@@ -1,6 +1,6 @@
 // PalmerPenguins.java
-// D. Singletary
-// 3/14/25
+// Brianna Geiger
+// 4/5/26
 // Palmer Penguins application
 
 package edu.fscj.cop2800c.penguin;
